7ee98031616608a7e71df9c539a198a1a54f0b79
Matthew Jadud <matt@jadud.com>
2019-03-13 00:26:52 -0400

    Adding in data for reference.
    To clean up tests.

--- operations.rkt
+++ operations.rkt
@@ -42,6 +42,7 @@
            table/reading/gsheet
            table/reading/csv
            table/test/files
+           table/test/data
            math)
   
   (define flavorsT (read-gsheet "https://pult.us/u/flavors"))
@@ -55,8 +56,9 @@
    (pull flavorsT "age") '(42 9 5)
 
    ;; What are the cities in the table?
-   (pull citiesT "City")
-   '("Youngstown" "Yankton" "Yakima" "Worcester" "Wisconsin Dells" "Winston-Salem" "Winnipeg" "Winchester" "Wilmington" "Wilmington" "Williston" "Williamsport" "Williamson" "Wichita Falls" "Wichita" "Wheeling" "West Palm Beach" "Wenatchee" "Weed" "Waycross" "Wausau" "Waukegan" "Watertown" "Watertown" "Waterloo" "Waterbury" "Washington" "Warren" "Walla Walla" "Waco" "Vincennes" "Victoria" "Vicksburg" "Vancouver" "Valley City" "Valdosta" "Utica" "Uniontown" "Tyler" "Twin Falls" "Tuscaloosa" "Tupelo" "Tulsa" "Tucson" "Trinidad" "Trenton" "Traverse City" "Toronto" "Topeka" "Toledo" "Texarkana" "Terre Haute" "Tampa" "Tallahassee" "Tacoma" "Syracuse" "Swainsboro" "Sumter" "Stroudsburg" "Stockton" "Stevens Point" "Steubenville" "Sterling" "Staunton" "Springfield" "Springfield" "Springfield" "Springfield" "Spokane" "South Bend" "Sioux Falls" "Sioux City" "Shreveport" "Sherman" "Sheridan" "Seminole" "Selma" "Sedalia" "Seattle" "Scranton" "Scottsbluff" "Schenectady" "Savannah" "Sault Sainte Marie" "Sarasota" "Santa Rosa" "Santa Fe" "Santa Barbara" "Santa Ana" "San Jose" "San Francisco" "Sandusky" "San Diego" "San Bernardino" "San Antonio" "San Angelo" "Salt Lake City" "Salisbury" "Salinas" "Salina" "Salida" "Salem" "Saint Paul" "Saint Louis" "Saint Joseph" "Saint Joseph" "Saint Johnsbury" "Saint Cloud" "Saint Augustine" "Saginaw" "Sacramento" "Rutland" "Roswell" "Rocky Mount" "Rock Springs" "Rockford" "Rochester" "Rochester" "Roanoke" "Richmond" "Richmond" "Richfield" "Rhinelander" "Reno" "Regina" "Red Bluff" "Reading" "Ravenna")
+   ;; Placed the list of cities in a test/data file.
+   (pull citiesT "City") all-cities
+   
 
    ;; What is the sum of the latitude degrees?
    ;; This checks that the integers came in as integers.
@@ -73,6 +75,10 @@
    ;; What about a bigger table?
    (length (pull gunsT 'age)) 100798
    (row-count gunsT) 100798
+   ;; The columns in this table are
+   ;; "col0","year","month","intent","police","sex","age","race","hispanic","place","education"
+   ;; Check if we can pick three from the table
+   (column-count (pick gunsT "year" "sex" "race")) 3
    
    ) ;; end of chk
 
