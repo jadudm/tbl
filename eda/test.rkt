c0872c48856b4061ddce15eb637ebae3be4929f4
Matthew Jadud <matt@jadud.com>
2019-03-13 10:33:10 -0400

    Found a non-working example.
    So, I need to be able to filter tables now.
    
    That is, I have NA values in my tables.
    
    I could have an import that will drop based on certain values; this is dangerous.
    
    So, I must implement filtering next.

--- eda/test.rkt
+++ eda/test.rkt
@@ -1,7 +1,12 @@
 #lang racket
 
 (require table/plot
-         table/reading/gsheet)
+         table/reading/gsheet
+         table/reading/csv
+         table/test/files)
 
 (define T (read-gsheet "http://bit.ly/2ChP8HY"))
+(define gunsT (read-csv gun-deaths-csv))
+
 (show (scatter T "LonD" "LatD"))
+(show (scatter gunsT "year" "age"))
