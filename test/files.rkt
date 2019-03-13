81e83f16103497e40a192dfec575c52b09fb2479
Matthew Jadud <matt@jadud.com>
2019-03-13 00:12:41 -0400

    Adding provenance.

--- test/files.rkt
+++ test/files.rkt
@@ -4,6 +4,12 @@
 
 (require racket/runtime-path)
 
+;; The guns.csv file is from
+;; https://raw.githubusercontent.com/fivethirtyeight/guns-data/master/full_data.csv
+;; which is part of the
+;; https://github.com/fivethirtyeight/guns-data
+;; repository which is part of the "Gun Deaths in America" project.
+;; https://fivethirtyeight.com/features/gun-deaths/
 (define-runtime-path gun-deaths-csv "guns.csv")
 
   
