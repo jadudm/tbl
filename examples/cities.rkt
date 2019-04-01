301af235b948c03edb745017623c20767b4865a5
Matthew Jadud <matt@jadud.com>
2019-04-01 19:09:49 -0400

    Updating the cities code to reflect current library.

--- examples/cities.rkt
+++ examples/cities.rkt
@@ -1,8 +1,8 @@
 #lang racket
 
-(require tbl/reading/gsheet
+(require tbl
          tbl/plot)
 
 ;; The source Google Sheet: http://bit.ly/cities-gsheet
 (define T (read-gsheet "http://bit.ly/cities-csv"))
-(show (scatter T "LonD" "LatD"))
+(scatterplot T "LonD" "LatD")
