7880cef4c32532c24472db83eb7f007ec92dba96
Matthew Jadud <matt@jadud.com>
2019-04-15 13:14:41 -0400

    Cleaning up tests.
    Need to get rid of the deps on visual.

--- operations.rkt
+++ operations.rkt
@@ -18,7 +18,10 @@
           
           [get-rows                     (-> tbl? (listof list?))]
           [get-rows-with-id             (-> tbl? (listof list?))]
+
+          [remove-columns               (->* (tbl?) () #:rest (listof string?) tbl?)]
           ))
+(provide filter-rows compute)
 
 ;; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ;
 ;; get-column
@@ -314,129 +317,3 @@
   (disconnect (tbl-db T))
   newT)
     
-
-;  ;;;;;;; ;;;;;;   ;;;;; ;;;;;;;  ;;;;; 
-;     ;    ;       ;     ;   ;    ;     ;
-;     ;    ;       ;     ;   ;    ;     ;
-;     ;    ;       ;         ;    ;      
-;     ;    ;;;;;;   ;;;;;    ;     ;;;;; 
-;     ;    ;             ;   ;          ;
-;     ;    ;       ;     ;   ;    ;     ;
-;     ;    ;       ;     ;   ;    ;     ;
-;     ;    ;;;;;;   ;;;;;    ;     ;;;;; 
-;                                        
-;                                        
-;                                        
-
-(module+ test
-  (require chk
-           tbl/reading/gsheet
-           tbl/reading/csv
-           tbl/test/files
-           tbl/test/data
-           math)
-  
-  (define flavorsT (read-gsheet "https://pult.us/u/flavors"))
-  (define citiesT (read-gsheet "http://bit.ly/cities-csv"))
- 
-  (chk
-   ;; What are the ages in the flavors GSheet?
-   (get-column flavorsT 'age)  '(42 9 5)
-   ;; What if I use a string for the column name?
-   (get-column flavorsT "age") '(42 9 5)
-
-   ;; What are the cities in the tbl?
-   ;; Placed the list of cities in a test/data file.
-   (get-column citiesT "City") all-cities
-   
-
-   ;; What is the sum of the latitude degrees?
-   ;; This checks that the integers came in as integers.
-   (apply + (get-column citiesT "LatD")) 4969
-   ;; How many times does Ohio show up in the tbl?
-   (length (filter (λ (s) (equal? s "OH")) (get-column citiesT "State"))) 6
-   ;; What about Maine? Oh. Maine gets no love.
-   (length (filter (λ (s) (equal? s "ME")) (get-column citiesT "State"))) 0
-
-   ;; Does pick return a table with fewer columns?
-   (count-columns flavorsT) 3
-   (count-columns (new-tbl-from-columns flavorsT "age" "name")) 2
-
-   ;; All the rows where a condition holds true.
-   ;; Note that ROWID is not included when we ask for all of the rows.
-   (get-rows (filter-rows flavorsT (and (> age 3)
-                                        (or (= flavor "Mint")
-                                            (= flavor "Chocolate")))))
-   '(("Matt" 42 "Chocolate") ("Matthew" 9 "Mint"))
-
-   ;; Another check; essentially same as the previous
-   (count-rows
-    (filter-rows flavorsT (and (> age 3)
-                               (or (= flavor "Mint")
-                                   (= flavor "Chocolate")))))
-   2
-   
-   ;; Check that we end up with a new table that only has
-   ;; six rows in it. It should still have 10 columns.
-   (count-rows (filter-rows citiesT (= State "OH"))) 6
-   (count-columns (filter-rows citiesT (= State "OH"))) 10
-   
-   ) ;; end of chk
-
-  ;; Try extending the table, and computing new values.
-  (quietly
-   (add-column flavorsT "double_age" Integer)
-   (compute flavorsT
-            "double_age"
-            (function (age) (* age 2))))
-
-  (chk
-   ;; FIXME When I handle params that are not named correctly,
-   ;; this test may need to be updated.
-   #:x (compute flavorsT
-                  "double_age"
-                  (function (agex) (* agex 2)))
-   " "
-   
-   (count-columns flavorsT) 4
-   ;; These do the same thing two different ways.
-   (map fourth (get-rows flavorsT)) '(84 18 10)
-   (get-column flavorsT "double_age") '(84 18 10)
-   )
-    
-  (define sdT (read-csv school-shootings))
-  
-  (chk
-   (count-rows sdT) 221
-   (count-rows (filter-rows sdT (> killed 0))) 59
-   )
-  
-  (define CT1 (read-gsheet "http://bit.ly/cities-csv"))
-  (define CT2 (remove-columns citiesT "NS" "EW" "State"))
-  (chk
-   (count-columns CT1) (length '(LatD LatM LatS NS LonD LonM LonS EW City State))
-   (count-columns CT2) (length '(LatD LatM LatS LonD LonM LonS City))
-   (set=? (map string->symbol (column-names CT1))
-               '(LatD LatM LatS NS LonD LonM LonS EW City State))
-   (set=? (map string->symbol (column-names CT2))
-               '(LatD LatM LatS LonD LonM LonS City)))
-  
-  ;; Using a bigger table
-  #;(begin
-      (define gunsT (read-csv gun-deaths-csv))
-      (chk
-     
-       ;; What about a bigger tbl?
-       (length (pull gunsT 'age)) 100798
-       (count-rows gunsT) 100798
-       ;; The columns in this table are
-       ;; "col0","year","month","intent","police","sex","age","race","hispanic","place","education"
-       ;; Check if we can pick three from the table
-       (count-columns (pick gunsT "year" "sex" "race")) 3
-       ))
-
-    
-  )
-
-;; Deleting the nth row
-;; https://stackoverflow.com/questions/23791239/sqlite-delete-nth-row-android
